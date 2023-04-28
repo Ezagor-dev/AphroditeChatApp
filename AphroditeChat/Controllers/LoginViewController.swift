37f97267562064c440499a47e8e3b4715d19b0a0
Ata <atakanozalan@gmail.com>
2023-04-28 17:12:48 +0300

    Creating a chatting app

---
+++ AphroditeChat/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  AphroditeChat
+//
+//  Created by Ezagor on 28.04.2023.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    
+}
---
+++ AphroditeChat/Controllers/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  AphroditeChat
+//
+//  Created by Ezagor on 28.04.2023.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+    }
+    
+}
