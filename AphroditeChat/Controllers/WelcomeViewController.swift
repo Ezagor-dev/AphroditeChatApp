d69e44eff7316167b89647fee313e71a3d961067
Ata <atakanozalan@gmail.com>
2023-05-01 16:27:37 +0300

    Installing pod

--- AphroditeChat/Controllers/WelcomeViewController.swift
+++ AphroditeChat/Controllers/WelcomeViewController.swift
@@ -14,6 +14,8 @@ class WelcomeViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // I installed https://cocoapods.org/ 
+        
         titleLabel.text = ""
         var charIndex = 0
         
