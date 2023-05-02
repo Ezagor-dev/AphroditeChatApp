a013deb7393a5eef0469e6470b15195a6598480c
Ata <atakanozalan@gmail.com>
2023-05-02 14:12:32 +0300

    Changing the class of a label from UILabel to CLTypingLabel

--- AphroditeChat/Controllers/WelcomeViewController.swift
+++ AphroditeChat/Controllers/WelcomeViewController.swift
@@ -6,29 +6,30 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         // I installed https://cocoapods.org/ 
         
-        titleLabel.text = ""
-        var charIndex = 0
-        
-        let titleText = "⚕Aphrodite"
-        for letter in titleText{
-            print(charIndex)
-            print(letter)
-            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { timer in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-            
-        }
+//        titleLabel.text = ""
+//        var charIndex = 0
+//
+//        let titleText = "⚕Aphrodite"
+//        for letter in titleText{
+//            print(charIndex)
+//            print(letter)
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(charIndex), repeats: false) { timer in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//
+//        }
         
     }
 
