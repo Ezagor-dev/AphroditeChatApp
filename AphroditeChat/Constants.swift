57dfd886f7e71ecf4869738291ff7fcedcb678ae
Ata <atakanozalan@gmail.com>
2023-05-10 20:10:49 +0300

    using constants file

---
+++ AphroditeChat/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  AphroditeChat
+//
+//  Created by Ezagor on 10.05.2023.
+//
+
+struct Constants {
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+}
--- AphroditeChat/Controllers/LoginViewController.swift
+++ AphroditeChat/Controllers/LoginViewController.swift
@@ -36,7 +36,7 @@ class LoginViewController: UIViewController {
                     
                 else{
                     //Navigate to the ChatViewController
-                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                    self.performSegue(withIdentifier: Constants.loginSegue, sender: self)
                 }
         }
         
