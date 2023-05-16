43b4a2b9151621d23159476d49e1fdbf3b588419
Ata <atakanozalan@gmail.com>
2023-05-16 23:14:14 +0300

    Firebase - Cloud Firestore Connection

--- AphroditeChat/Controllers/ChatViewController.swift
+++ AphroditeChat/Controllers/ChatViewController.swift
@@ -13,6 +13,8 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let db = Firestore.firestore()
+    
     var messages: [Message] = [
         Message(sender: "ezagor@icloud.com", body: "Hey!"),
         Message(sender: "atakanozalan@gmail.com", body: "Hello!"),
@@ -26,10 +28,41 @@ class ChatViewController: UIViewController {
         navigationItem.hidesBackButton = true
         
         tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
+        
+        loadMessages()
 
     }
     
+    func loadMessages(){
+        messages = []
+        
+        db.collection(K.FStore.collectionName).getDocuments { querySnapshot, error in
+            if let e = error{
+                print("There was an issue retrieving data from Firestore. \(e)")
+            }else{
+                if let snapshotDocuments = querySnapshot?.documents{
+                    for doc in snapshotDocuments{
+                        print(doc.data())
+                    }
+                }
+            }
+        }
+    }
+    
     @IBAction func sendPressed(_ sender: UIButton) {
+        
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody
+            ]){ (error) in
+                if let e = error{
+                    print("There was an issue saving data to firestore, \(e)")
+                }else{
+                    print("Successfully saved data.")
+                }
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
